987dcf90b6c44d908aaf2f96fde6739df923f8d9
FatSassin <hubert@talwik.com>
2024-04-08 10:10:02 +0200

    agagagaga

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-hello world my name is john and i eat worms
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Gunsitedotnet</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1>SUPER JAJA</h1>
+        </body>
+        <?php
+         echo "shrek";
+         echo "<br>";
+         echo "shrek 2";
+        ?>
+    </html>
