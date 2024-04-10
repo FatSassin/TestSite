7d69ab319c417c0e5e808994dfbd150f885b11c8
FatSassin <hubert@talwik.com>
2024-04-10 14:59:35 +0200

    amongu

--- index.php
+++ index.php
@@ -6,10 +6,36 @@
         </head>
         <body>
             <h1>SUPER JAJA</h1>
+            <form action="index.php" method="POST">
+                Name: <input type="text" name="name"><br>
+                <p><input type="submit" name="submit" value="submit" /></p>
+            </form>
         </body>
         <?php
-         echo "shrek";
-         echo "<br>";
-         echo "shrek 2";
+            // Create connection
+            $conn = new mysqli('localhost', 'root', '', 'gunsitedotpng');
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            echo "Connected successfully";
+
+            echo "<br>";
+            if (isset( $_POST['name'])){
+
+                $name = $_POST['name'];
+                $sql = "INSERT INTO countries (name) VALUES (\"".$name."\")";
+
+                echo "$sql";
+                echo "<br>";
+                if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+                } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+                $conn->close();
+            }
+            echo "<br>";
+            $query = $mysqli->query("SELECT * FROM countries"); 
         ?>
     </html>
