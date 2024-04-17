8dae6add962823f9241948b2be761a1d264cf91b
FatSassin <hubert@talwik.com>
2024-04-17 13:46:52 +0200

    basic additions

--- subsites/country.php
+++ subsites/country.php
@@ -14,12 +14,6 @@
         <?php
             // Create connection
             $conn = new mysqli('localhost', 'root', '', 'gunsitedotpng');
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-            echo "Connected successfully";
-
             echo "<br>";
             if (isset( $_POST['name'])){
 
