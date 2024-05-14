8eb7ae7ae73eb1af6221fe802613b92b92001179
FatSassin <hubert@talwik.com>
2024-05-14 09:49:58 +0200

    amongus

--- subsites/showreview.php
+++ subsites/showreview.php
@@ -1,37 +1,33 @@
 <html>
-<body>
-<?php 
-$mysqli = new mysqli("localhost", "root", "", "gunsitedotpng"); 
-$query = "SELECT * FROM recenzje";
+    <body>
+    <?php 
+    $mysqli = new mysqli("localhost", "root", "", "gunsitedotpng"); 
+    $query = "SELECT * FROM recenzje";
 
+    echo '<table border="0" cellspacing="2" cellpadding="2"> 
+        <tr> 
+            <td> <font face="Arial">User</font> </td> 
+            <td> <font face="Arial">Weapon</font> </td> 
+            <td> <font face="Arial">Review</font> </td> 
+            <td> <font face="Arial">Rating (Max 10)</font> </td> 
+        </tr>';
 
-echo '<table border="0" cellspacing="2" cellpadding="2"> 
-      <tr> 
-          <td> <font face="Arial">Value1</font> </td> 
-          <td> <font face="Arial">Value2</font> </td> 
-          <td> <font face="Arial">Value3</font> </td> 
-          <td> <font face="Arial">Value4</font> </td> 
-          <td> <font face="Arial">Value5</font> </td> 
-      </tr>';
+    if ($result = $mysqli->query($query)) {
+        while ($row = $result->fetch_assoc()) {
+            $field2name = $row["user_id"];
+            $field3name = $row["gun_id"];
+            $field4name = $row["text"];
+            $field5name = $row["ocena"]; 
 
-if ($result = $mysqli->query($query)) {
-    while ($row = $result->fetch_assoc()) {
-        $field1name = $row["review_id"];
-        $field2name = $row["user_id"];
-        $field3name = $row["gun_id"];
-        $field4name = $row["text"];
-        $field5name = $row["ocena"]; 
-
-        echo '<tr> 
-                  <td>'.$field1name.'</td> 
-                  <td>'.$field2name.'</td> 
-                  <td>'.$field3name.'</td> 
-                  <td>'.$field4name.'</td> 
-                  <td>'.$field5name.'</td> 
-              </tr>';
-    }
-    $result->free();
-} 
-?>
-</body>
+            echo '<tr> 
+                    <td>'.$field2name.'</td> 
+                    <td>'.$field3name.'</td> 
+                    <td>'.$field4name.'</td> 
+                    <td>'.$field5name.'</td> 
+                </tr>';
+        }
+        $result->free();
+    } 
+    ?>
+    </body>
 </html>
